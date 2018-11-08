5d89cdaf7465e51783e058ad8414b64d1893af69
Mike <grossmi4@gmail.com>
2018-11-08 15:46:12 -0500

    basic functionality working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,14 +20,14 @@ const initialize = {
   },
   getItems: function() {
     // query to pull all items from products table in bamazon DB
-    connection.query('SELECT item_id, product_name, price FROM products ORDER BY item_id ASC;', function(err,res){
+    connection.query('SELECT item_id, product_name, price, stock_quantity FROM products ORDER BY item_id ASC;', function(err,res){
 
       //if an error occurs, throw the error to the console
       if (err) throw err;
 
       //cycle through all received responses and append them to the items array to create list used in prompt
       for (let i = 0; i < res.length; i++) {
-        initialize.items[i] = [res[i].item_id, `${res[i].product_name} - $${res[i].price}`];
+        initialize.items[i] = [res[i].item_id, `${res[i].product_name} - $${res[i].price}`,res[i].stock_quantity];
       }
 
       //after receiving items, run prompt with built array
@@ -47,8 +47,8 @@ const initialize = {
 
     //once user has made a selection, it is passed into the store method qtyPrompt
         .then(selection => {
-          store.selectedItemId = arrayExtractor(initialize.items,1).indexOf(selection.product);
-          store.qtyPrompt(store.selectedItemId);
+          store.selectedItemIndex = arrayExtractor(initialize.items, 1).indexOf(selection.product);
+          store.qtyPrompt(store.selectedItemIndex);
         })
         .catch(err => {
           console.log(err)
@@ -59,9 +59,10 @@ const initialize = {
 //object containing all methods to handle purchase of an item
 let store = {
   QoH: 0,
-  selectedItemId: -1,
-  qtyPrompt: function (itemId) {
-    store.QoH = store.qtyCheck(itemId);
+  selectedItemIndex: -1,
+  qtyPrompt: function (itemIndex) {
+    console.log('checking quantity');
+    store.qtyCheck(store.selectedItemIndex);
     if (store.QoH < 1) {
       console.log("We are out of stock for that item!");
       process.exit();
@@ -83,9 +84,10 @@ let store = {
     ])
         .then(input => {
           console.log('processing transaction...');
-          console.log(store.QoH);
           let qty = parseInt(input.qty,10);
           if(store.QoH > qty) {
+            let itemId = initialize.items[itemIndex][0];
+            console.log(itemId);
             store.processTransaction(itemId,(store.QoH - qty))
           }
         })
@@ -93,19 +95,9 @@ let store = {
           console.log(err)
         })
   },
-  qtyCheck: function (itemId) {
-    connection.query(
-        'SELECT stock_quantity FROM products WHERE item_id = ?',
-        itemId,
-        function (err, res) {
-
-          //if an error occurs, throw the error to the console
-          if (err) throw err;
-
-          console.log(res.stock_quantity);
-
-          return res.stock_quantity;
-        })
+  qtyCheck: function (itemIndex) {
+    let newQoH = initialize.items[itemIndex][2];
+    store.QoH = newQoH;
   },
 
   processTransaction: function(itemId, qty) {
@@ -114,7 +106,8 @@ let store = {
         [qty, itemId],
         function (err, res) {
           if (err) throw err;
-          return res
+          console.log(`${res.affectedRows} rows updated`);
+          console.log('order successfully placed!')
         }
     )
   }
